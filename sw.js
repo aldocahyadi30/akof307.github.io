0c2c6869b113c5fecf0059da6ccfb42ab47bac95
akof307 <78845052+akof307@users.noreply.github.com>
2023-03-31 01:27:35 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,41 +1,19 @@
 self.addEventListener('install', function(event) {
-    event.waitUntil(
-      caches.open('first-app')
-        .then(function(cache) {
-          cache.addAll([
-            '/',
-            '/index.html',
-            '/src/css/app.css',
-            '/src/js/app.js'
-          ])
-        })
-    );
-    return self.clients.claim();
-  });
-  
-  self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      caches.match(event.request)
-        .then(function(res) {
-          return res;
-        })
-    );
-  });
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  event.waitUntil(
+    caches.open('static')
+      .then(function(cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.add('/src/js/app.js')
+      })
+  )
+});
 
 self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
-  event.waitUntil(
-    caches.keys()
-      .then(function(keyList) {
-        return Promise.all(keyList.map(function(key) {
-          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
-            console.log('[Service Worker] Removing old cache.', key);
-            return caches.delete(key);
-          }
-        }));
-      })
-  );
   return self.clients.claim();
 });
 
-  
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
+});
