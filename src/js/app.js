d273eaf613d8fb1c28ef1e1da987bb673e2dd5c3
akof307 <78845052+akof307@users.noreply.github.com>
2023-03-31 01:45:11 +0700

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,13 +4,13 @@ var button = document.querySelector('button');
 
 navigator.serviceWorker.register('/sw.js');
 
-var deferredPrompt;
-window.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
-  event.preventDefault();
-  deferredPrompt = event;
-  return false;
-});
+// var deferredPrompt;
+// window.addEventListener('beforeinstallprompt', function(event) {
+//   console.log('beforeinstallprompt fired');
+//   event.preventDefault();
+//   deferredPrompt = event;
+//   return false;
+// });
 
 function animate() {
   title.classList.remove('animate-in');
