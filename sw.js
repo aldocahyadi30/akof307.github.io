fcfebafe029a3d1a4b850ea9c0d3ac38a8321c2c
akof307 <78845052+akof307@users.noreply.github.com>
2023-03-31 07:54:27 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,48 @@
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
+  self.skipWaiting(); //PENTING bila ada versi baru!!
+  event.waitUntil(
+    caches.open('static')
+      .then(function(cache) {
+        console.log('[Service Worker] Precaching App Shell');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/app.js',
+          '/blog.html',
+          '/about.html',
+          '/style.css',
+          '/contact.html'
+        ]);
+      })
+  )
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keyList) {
+        return Promise.all(keyList.map(function(key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log('[Service Worker] Removing old cache.', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.open(CACHE_DYNAMIC_NAME)
+      .then(function(cache) {
+        return fetch(event.request)
+          .then(function(res) {
+            cache.put(event.request, res.clone());
+            return res;
+          });
+      })
+  );
+});
